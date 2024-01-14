823e5842375a376d6397750d437dba0eaec8cc8b
Théo Thoumine <theo@thoumine.de>
2024-01-14 02:34:40 +0100

    Add additional visualizations and functionality

--- app.R
+++ app.R
@@ -10,6 +10,36 @@
 
 library(shiny)
 
+options_nominal <- c(
+  "Geschlecht",
+  "Klasse",
+  "Einstiegshafen"
+)
+options_kardinal <- c(
+  "Alter",
+  "Kinder und Eltern an Bord",
+  "Geschwister und Ehepartner an Bord",
+  "Ticketpreis"
+)
+options <- c(options_nominal, options_kardinal)
+
+switch_options <- function(data, selected_option, breaks = NULL) {
+
+  cabin_letter <- gsub("[0-9]", "", data$Cabin)
+
+  switch(selected_option,
+    "Alter" = data$Age,
+    "Klasse" = data$Pclass,
+    "Geschlecht" = data$Sex,
+    "Kinder und Eltern an Bord" = data$Parch,
+    "Geschwister und Ehepartner an Bord" = data$SibSp,
+    "Einstiegshafen" = data$Embarked,
+    "Ticketpreis" = data$Fare,
+    "Kabinenbuchstabe" = cabin_letter
+  )
+}
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   navbarPage(
@@ -27,10 +57,9 @@ ui <- fluidPage(
           )
         ),
         mainPanel(
-          plotOutput("survivalClassBarplot"),
-          "Altersverteilung der Passagiere",
+          plotOutput("survival"),
           plotOutput("altersverteilungHist"),
-          tableOutput("altersverteilungRelHäufigkeiten")
+          plotOutput("survivalClassBarplot"),
         )
       )
     ),
@@ -40,60 +69,79 @@ ui <- fluidPage(
         "nach Überleben",
         sidebarLayout(
           sidebarPanel(
-            # dropdown menu for second dimension of age, class etc.
             selectInput(
               "additionalDimension",
               "zweite Dimension:",
-              choices = c(
-                "Alter",
-                "Klasse",
-                "Geschlecht",
-                "Kinder und Eltern an Bord",
-                "Geschwister und Ehepartner an Bord",
-                "Hafen der Einschiffung",
-                "Ticketpreis"
-              ),
+              choices = options,
               multiple = FALSE
             ),
             uiOutput("slider")
           ),
           mainPanel(
-            plotOutput("two_dim_mosaic")
+            plotOutput("two_dim_mosaic"),
+            plotOutput("abhängigkeit")
           )
         )
       ),
       tabPanel(
-        "Kombination zweier Merkmale",
+        "Kombination im Streudiagramm",
         sidebarLayout(
           sidebarPanel(
             selectInput(
-              "firstDimension",
+              "first_dimension_scatter",
               "erste Dimension:",
-              choices = c(
-                "Alter",
-                "Klasse",
-                "Kinder und Eltern an Bord",
-                "Geschwister und Ehepartner an Bord",
-                "Ticketpreis"
-              ),
+              choices = options_kardinal,
               multiple = FALSE
             ),
             selectInput(
-              "secondDimension",
+              "second_dimension_scatter",
               "zweite Dimension:",
-              choices = c(
-                "Alter",
-                "Klasse",
-                "Kinder und Eltern an Bord",
-                "Geschwister und Ehepartner an Bord",
-                "Ticketpreis"
-              ),
+              choices = options_kardinal,
               multiple = FALSE
             )
           ),
           mainPanel(
-            plotOutput("scatterplot")
+            plotOutput("scatterplot"),
           )
+        )
+      ),
+      tabPanel(
+        "Kombination im Mosaikdiagramm",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              "first_dimension_mosaic",
+              "erste Dimension:",
+              choices = options_nominal,
+              multiple = FALSE
+            ),
+            selectInput(
+              "second_dimension_mosaic",
+              "zweite Dimension:",
+              choices = options_nominal,
+              multiple = FALSE
+            )
+          ),
+          mainPanel(
+            plotOutput("mosaicplot")
+          )
+        )
+      )
+    ),
+    tabPanel(
+      "Kontingenzmaße",
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(
+            "contingencyDimensions",
+            "Merkmale:",
+            choices = options,
+            multiple = TRUE,
+            selected = c("Alter", "Klasse")
+          )
+        ),
+        mainPanel(
+          plotOutput("contingency")
         )
       )
     )
@@ -103,33 +151,36 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   library(knitr)
+  library(DescTools)
   data <- read.csv2("titanic_data.csv", sep = ",", dec = ".")
   data$Survived <- as.logical(data$Survived)
 
 
   ######################################
 
-  output$survivalClassBarplot <- renderPlot({
-    # convert 0 and 1 to boolean
-    data$Survived <- as.logical(data$Survived)
+  output$survival <- renderPlot({
+    barplot(
+      prop.table(table(data$Survived)),
+      main = "Überlebensstatistik",
+      ylab = "Überlebt",
+      xlab = "Relative Häufigkeit",
+      names.arg = c("Gestorben", "Überlebt"),
+      horiz = TRUE,
+      col = c("#ff000053", "#00ff003e")
+    )
+  })
 
-    # draw the histogram with the specified number of bins
+  output$survivalClassBarplot <- renderPlot({
     prop_survival_class <- prop.table(table(data$Survived, data$Pclass), 1)
     library(knitr)
     kable(prop_survival_class)
     barplot(prop_survival_class,
-      main = "Überlebensstatistik"
+      main = "Kabinenklasse mit Anteilen der Überlebenden",
     )
   })
 
-  output$altersverteilungRelHäufigkeiten <- renderTable({
-    breaks <- seq(0, 100, input$gruppen)
-    Altersgruppen <- cut(data$Age, breaks = breaks)
-    prop.table(table(Altersgruppen)) * 100
-  })
 
   output$altersverteilungHist <- renderPlot({
-    # Histogramm der Altersgruppen
     breaks <- seq(0, 100, input$gruppen)
     hist(
       data$Age,
@@ -143,17 +194,11 @@ server <- function(input, output) {
 
 
   data_selected_dim <- reactive({
-    cabin_letter <- gsub("[0-9]", "", data$Cabin)
-    switch(input$additionalDimension,
-      "Alter" = cut(data$Age, breaks = 8),
-      "Klasse" = data$Pclass,
-      "Geschlecht" = data$Sex,
-      "Kinder und Eltern an Bord" = data$Parch,
-      "Geschwister und Ehepartner an Bord" = data$SibSp,
-      "Hafen der Einschiffung" = data$Embarked,
-      "Ticketpreis" = cut(data$Fare, breaks = 15),
-    )
+    switch_options(data, input$additionalDimension)
   })
+
+
+  ######################################
 
   output$two_dim_mosaic <- renderPlot({
     mosaicplot(
@@ -161,47 +206,89 @@ server <- function(input, output) {
         data_selected_dim(),
         data$Survived
       ),
-      main = "Überlebensstatistik",
+      main = paste(
+        "Mosaikplot Überleben in Abhängigkeit von",
+        input$additionalDimension
+      ),
       xlab = input$additionalDimension,
       ylab = "Überlebt",
       color = TRUE
     )
-  }) 
-
-
-  
-  ######################################
-  data_first_selected_dim <- reactive({
-    cabin_letter <- gsub("[0-9]", "", data$Cabin)
-    switch(input$firstDimension,
-      "Alter" = data$Age,
-      "Klasse" = data$Pclass,
-      "Kinder und Eltern an Bord" = data$Parch,
-      "Geschwister und Ehepartner an Bord" = data$SibSp,
-      "Ticketpreis" = data$Fare,
-    )
   })
 
+  ######################################
+
+  data_first_selected_dim <- reactive({
+    switch_options(data, input$first_dimension_scatter)
+  })
   data_second_selected_dim <- reactive({
-    cabin_letter <- gsub("[0-9]", "", data$Cabin)
-    switch(input$secondDimension,
-      "Alter" = data$Age,
-      "Klasse" = data$Pclass,
-      "Kinder und Eltern an Bord" = data$Parch,
-      "Geschwister und Ehepartner an Bord" = data$SibSp,
-      "Ticketpreis" = data$Fare,
-    )
+    switch_options(data, input$second_dimension_scatter)
   })
 
   output$scatterplot <- renderPlot(
     plot(
       data_first_selected_dim(),
       data_second_selected_dim(),
-      main = "Überlebensstatistik",
-      xlab = input$firstDimension,
-      ylab = input$secondDimension
-    )
+      main = paste(
+        "Streudiagramm von",
+        input$first_dimension_scatter,
+        "und",
+        input$second_dimension_scatter
+      ),
+      xlab = input$first_dimension_scatter,
+      ylab = input$second_dimension_scatter
+    ),
   )
+
+#######################################
+
+
+  data_first_selected_dim_mosaic <- reactive({
+    switch_options(data, input$first_dimension_mosaic)
+  })
+
+  data_second_selected_dim_mosaic <- reactive({
+    switch_options(data, input$second_dimension_mosaic)
+  })
+
+  output$mosaicplot <- renderPlot({
+    mosaicplot(
+      table(
+        data_first_selected_dim_mosaic(),
+        data_second_selected_dim_mosaic()
+      ),
+      main = paste(
+        "Mosaikplot",
+        input$first_dimension_mosaic,
+        "in Abhängigkeit von",
+        input$second_dimension_mosaic
+      ),
+      xlab = input$first_dimension_mosaic,
+      ylab = input$second_dimension_mosaic,
+      color = TRUE
+    )
+  })
+
+  #######################################
+
+
+  output$contingency <- renderPlot({
+    cramer_v_values <- sapply(input$contingencyDimensions, function(dim) {
+      selected_dim <- switch_options(data, dim)
+      CramerV(data$Survived, selected_dim)
+    })
+
+    barplot(
+      cramer_v_values,
+      xlab = "Cramer V",
+      ylab = "Dimension",
+      main = "Kontingenzmaße vom Überleben (Cramer V)",
+      xlim = c(0, 1),
+      horiz = TRUE
+    )
+  })
+
+  ######################################
 }
 
 # Run the application
